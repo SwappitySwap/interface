cecf53ec24cdb76e0ca0f8a8b722077c1613dff3
swappityswap-dev <swappityswaps@gmail.com>
2021-11-11 16:05:41 -0500

    swappityswap

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,16 @@
+import { ChainId } from '@swappityswap-dev/sdk'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+  [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+  [ChainId.GÖRLI]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e',
+  [ChainId.BSC]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb',
+  [ChainId.XDAI]: '0x0aeba5eCf6dbA04ce40D5EDE55723354aC960237',
+
+
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
